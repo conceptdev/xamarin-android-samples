f0eee21e7fdaeaaf370427ef2ad0982dd41b36c8
Craig Dunn <craig.dunn@conceptdevelopment.net>
2015-04-22 10:49:17 -0700

    [wear8ball] add graphics and randomize code

--- Wear8Ball/Wear8Ball/MainActivity.cs
+++ Wear8Ball/Wear8Ball/MainActivity.cs
@@ -16,56 +16,55 @@ using Export = Java.Interop.ExportAttribute;
 namespace wear8ball
 {
 	[Activity (Label = "Wear8Ball", MainLauncher = true, Icon = "@drawable/ic_launcher")]
-	[IntentFilter (new string[]{ "wear8ball.GridExampleActivity" }, 
-		Categories = new string[]{ "android.intent.category.DEFAULT" })]
-	public class MainActivity : Activity, DelayedConfirmationView.IDelayedConfirmationListener
+	public class MainActivity : Activity
 	{
-		private static string TAG = "MainActivity";
-
-		private static int NOTIFICATION_ID = 1;
-		private static int NOTIFICATION_REQUEST_CODE = 1;
-		private static int NUM_SECONDS = 5;
-
-		private GestureDetectorCompat mGestureDetector;
-		internal DismissOverlayView mDismissOverlayView;
+		Button button;
+		TextView result;
+		string lastResult = "";
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
 			SetContentView (Resource.Layout.main_activity);
 
-			mDismissOverlayView = FindViewById<DismissOverlayView> (Resource.Id.dismiss_overlay);
-			mDismissOverlayView.SetIntroText (Resource.String.intro_text);
-			mDismissOverlayView.ShowIntroIfNecessary ();
-			mGestureDetector = new GestureDetectorCompat (this, new LongPressListener (this));
+			button = FindViewById<Button> (Resource.Id.shake);
+			result = FindViewById<TextView> (Resource.Id.result);
+
+			button.Click += (sender, e) => {
+				var rnd = new System.Random();
+				lastResult = options[rnd.Next(0, options.Length - 1)];
+
+				result.Text = lastResult;
+			};
 		}
 
-		public override bool DispatchTouchEvent (MotionEvent ev)
-		{
-			return mGestureDetector.OnTouchEvent (ev) || base.DispatchTouchEvent (ev);
-		}
+		string[] options = {
+			"It is certain"
+			, "It is decidedly so"
+			, "Without a doubt"
+			, "Yes definitely"
+			, "You may rely on it"
+			, "As I see it, yes"
+			, "Most likely"
+			, "Outlook good"
+			, "Yes"
+			, "Signs point to yes"
 
-		/**
-		* Handles the button to launch a notification.
-		*/
-		[Export ("showNotification")]
-		public void ShowNotification (View view)
-		{
-//			var notification = new NotificationCompat.Builder (this)
-//				.SetContentTitle (GetString (Resource.String.notification_title))
-//				.SetContentText (GetString (Resource.String.notification_title))
-//				.SetSmallIcon (Resource.Drawable.ic_launcher)
-//				.AddAction (Resource.Drawable.ic_launcher,
-//				                   GetText (Resource.String.action_launch_activity),
-//				                   PendingIntent.GetActivity (this, NOTIFICATION_REQUEST_CODE,
-//					                   new Intent (this, typeof(GridExampleActivity)),
-//					                   PendingIntentFlags.UpdateCurrent))
-//				.Build ();
-//			NotificationManagerCompat.From (this).Notify (NOTIFICATION_ID, notification);
-			Finish ();
-		}
+			, "Reply hazy try again"
+			, "Ask again later"
+			, "Better not tell you now"
+			, "Cannot predict now"
+			, "Concentrate and ask again"
 
-		/**
+			, "Don't count on it"
+			, "My reply is no"
+			, "My sources say no"
+			, "Outlook not so good"
+			, "Very doubtful"
+		};
+	
+
+	/**
 	 * Handles the button press to finish this activity and take the user back to the Home.
 	 */
 		[Export ("onFinishActivity")]
@@ -74,55 +73,5 @@ namespace wear8ball
 			SetResult (Result.Ok);
 			Finish ();
 		}
-
-		/**
-	 * Handles the button to start a DelayedConfirmationView timer.
-	 */
-		[Export ("onStartTimer")]
-		public void OnStartTimer (View v)
-		{
-			var delayedConfirmationView = FindViewById<DelayedConfirmationView> (Resource.Id.timer);
-			delayedConfirmationView.SetTotalTimeMs (NUM_SECONDS * 1000);
-			delayedConfirmationView.SetListener (this);
-			delayedConfirmationView.Start ();
-			Scroll (FocusSearchDirection.Down);
-		}
-
-		public void OnTimerSelected (View v)
-		{
-			Log.Debug (TAG, "OnTimerSelected called");
-			Scroll (FocusSearchDirection.Up);
-		}
-
-		public void OnTimerFinished (View v)
-		{
-			Log.Debug (TAG, "OnTimerFinished called");
-			Scroll (FocusSearchDirection.Up);
-		}
-
-		private void Scroll (FocusSearchDirection scrollDirection)
-		{
-			var scrollView = FindViewById<ScrollView> (Resource.Id.scroll);
-			scrollView.Post (() => {
-				scrollView.FullScroll (scrollDirection);
-			});
-		}
-	}
-
-	internal class LongPressListener : GestureDetector.SimpleOnGestureListener
-	{
-		MainActivity owner;
-
-		public LongPressListener (MainActivity owner)
-		{
-			this.owner = owner;
-		}
-
-		public override void OnLongPress (MotionEvent e)
-		{
-			owner.mDismissOverlayView.Show ();
-		}
 	}
 }
-
-
